10a86ba9a64e5fb3a6d963d174fb612d075728c3
MJoon-Jung <gjgjajaj31@gmail.com>
2021-10-06 23:26:05 +0900

    [UPDATE] route

--- BuyingAndSellingUsedStuff/backend/routes/web.php
+++ BuyingAndSellingUsedStuff/backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Request as FacadesRequest;
+use App\Domains\Post\Controllers\PostController;
+use App\Domains\User\Controller\UserController;
+use App\Domains\User\Controller\AuthController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\GroupUserController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\RoomUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,22 +22,94 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-// Route::get('api/auth/login', [AuthController::class, "redirectToProvider"])->name('login');
-// Route::get('api/auth/login/callback', [AuthController::class, "handleProviderCallback"]);
+//Auth
+Route::group([
+    'prefix' => 'api/auth/login',
+    'middleware' => 'guest',
+], function () {
+    Route::get('/', [AuthController::class, "redirectToProvider"])->name('login');
+    Route::get('/callback', [AuthController::class, "handleProviderCallback"]);
+});
 
 Route::group([
     'prefix' => 'api/auth',
-], function ($router) {
+    'middleware' => 'auth',
+], function () {
+    Route::post('/logout', [AuthController::class, "logout"]);
+});
+
+//User
+Route::group([
+    'prefix' => 'api/users',
+    'middleware' => 'auth',
+], function () {
+    Route::get('/', [UserController::class, "index"]);
+    Route::get('/{id}', [UserController::class, "show"]);
+    Route::post('/profile', [UserController::class, "update"]);
+    Route::delete('/{id}', [UserController::class, "destroy"]);
+
+    //Friend
     Route::group([
-        'prefix' => 'login',
-        'middleware' => 'guest',
-    ], function ($router) {
-        Route::get('/', [AuthController::class, "redirectToProvider"])->name('login');
-        Route::get('/callback', [AuthController::class, "handleProviderCallback"]);
+        'prefix' => 'friends',
+    ], function () {
+        Route::get('/', [FriendController::class, "index"]);
+        Route::post('/', [FriendController::class, "store"]);
+        Route::delete('/', [FriendController::class, "destroy"]);
     });
+});
+
+//Post
+Route::group([
+    'prefix' => 'api/posts',
+    'middleware' => 'auth',
+], function () {
+    Route::get('/', [PostController::class, "index"]);
+    Route::get('/{id}', [PostController::class, "show"]);
+    Route::post('/', [PostController::class, "store"]);
+    Route::patch('/{id}', [PostController::class, "update"]);
+    Route::patch('/{id}/like', [PostController::class, "like"]);
+    Route::delete('/{id}', [PostController::class, "destroy"]);
+    Route::delete('/{id}/like', [PostController::class, "unlike"]);
+});
+
+//Group
+Route::group([
+    'prefix' => 'api/groups',
+    'middleware' => 'auth',
+], function () {
+    Route::get('/', [GroupController::class, "index"]);
+    Route::get('/{id}', [GroupController::class, "show"]);
+    Route::post('/', [GroupController::class, "store"]);
+    Route::patch('/{id}', [GroupController::class, "update"]);
+    Route::delete('/{id}', [GroupController::class, "destroy"]);
     Route::group([
-        'middleware' => 'auth',
-    ], function ($router) {
-        Route::post('/logout', [AuthController::class, "logout"]);
+        'prefix' => 'users',
+    ], function () {
+        Route::get('/', [GroupUserController::class, "index"]);
+        Route::get('/{user_id}', [GroupUserController::class, "show"]);
+        Route::post('/', [GroupUserController::class, "store"]);
+        Route::patch('/{user_id}', [GroupUserController::class, "update"]);
+        Route::delete('/{user_id}', [GroupUserController::class, "destroy"]);
+    });
+});
+
+//Room
+Route::group([
+    'prefix' => 'api/rooms',
+    'middleware' => 'auth',
+], function () {
+    Route::get('/', [RoomController::class, "index"]);
+    Route::get('/{id}', [RoomController::class, "show"]);
+    Route::post('/', [RoomController::class, "store"]);
+    Route::patch('/{id}', [RoomController::class, "update"]);
+    Route::delete('/{id}', [RoomController::class, "destroy"]);
+    Route::group([
+        'prefix' => 'users',
+    ], function () {
+        Route::get('/', [RoomUserController::class, "index"]);
+        Route::get('/{id}', [RoomUserController::class, "show"]);
+        Route::post('/', [RoomUserController::class, "store"]);
+        Route::patch('/{id}', [RoomUserController::class, "update"]);
+        Route::delete('/{id}', [RoomUserController::class, "destroy"]);
     });
 });
